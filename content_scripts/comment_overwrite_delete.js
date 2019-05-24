81c47b86e615a0646891298662c80199a75fa851
Sree <sr33@users.noreply.github.com>
2019-05-24 14:14:25 -0400

    del UserProfile compatibility check

--- content_scripts/comment_overwrite_delete.js
+++ content_scripts/comment_overwrite_delete.js
@@ -2,7 +2,4 @@
 console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n\nN U K E + R E D D I T + H I S T O R Y\n\n<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
 
 var userPage = new UserPage();
-
-// Check User Profile Compatibility before init
-if (userPage.isUserProfileCompatible) userPage.init();
-else alert('This format of user profile page is incompatible with Nuke Reddit History.\nPlease goto https://www.reddit.com/profile-beta-confirmation to get a newer reddit profile')
+userPage.init();
